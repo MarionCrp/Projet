2462688aba0cc2e2afeff1b0bec80b9fd40290e9
Marion <marion_craipeau@hotmail.fr>
2016-01-14 13:21:51 +0100

    Ajout du repository View

--- index.php
+++ index.php
@@ -10,34 +10,49 @@ include_once('autoload.php');
 	<!-- <meta charset="ISO-8859-1">  -->
 	<meta charset="utf-8"/>
 	<meta http-equiv="Content-type" content="text/html; charset=iso-8859-1" />
-	<link rel="stylesheet" href="css/css/bootstrap.css"/>
-	<link rel="stylesheet" href="css/css/style.css"/>
+
+	<link rel="stylesheet" href="assets/css/css/bootstrap.css"/>
+		<link rel="stylesheet" href="assets/css/css/style.css"/>
 	<title>World & I </title>
 </head>
 
 	<body>
 
-	<header>
+<div class="container-fluid">
+  <div class="row">
+  <div class="col-md-1"><img src="assets/images/logo.png" /></div>
+  <div class="col-md-10"></div>
+  <div class="col-md-1">.col-md-1</div>
+  <div class="col-md-1"><img src="assets/images/spain_flag.png" /></div>
+  </div>
+</div>
+
+	<div class="page-header">
 	<h1 style="text-align: center;"> CityWorld </h1>
-	</header>
+	</div>
 
-	<?php
+	<div class="jumbotron">
+		<div class="container">
+			<?php
 
 
-	if(isset($_GET['page'])) 
-	{
-		$page = $_GET['page'];
-	}
+			if(isset($_GET['page'])) 
+			{
+				$page = $_GET['page'];
+			}
 
-	else 
-	{
-		$page = 'home';
-	}
+			else 
+			{
+				$page = 'home';
+			}
 
-	include ($page.'.php');
+			include ('view/'.$page.'.php');
 
-	?>
+			?>
+		</div>
+	</div>
 
+	
 	</body>
 
 
