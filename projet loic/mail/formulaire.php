8746d8bc37f882cff10bcb6e06a36f19ed86d9da
BRONNER <bronnerloic@gmail.com>
2016-01-24 12:26:52 +0100

    prohgrammes pour le mail de validation

---
+++ projet loic/mail/formulaire.php
@@ -0,0 +1,31 @@
+<html>
+
+<head>
+<title>verification de compte</title>
+</head>
+
+<body>
+
+Saisir votre @email pour recevoir un mail de verification de compte : <br>
+<form method="post" action="envoimail.php">
+   <p>
+       <label for="pseudo">Votre pseudo :</label>
+       <input type="text" name="pseudo" id="pseudo" placeholder="rien" />
+
+	   <br />
+	   <br />
+	   
+       <label for="votremail">Votre adresse mail :</label>
+       <input type="email" name="votremail" id="votremail" placeholder="rien@rien.fr" />
+
+	   <br />
+	   <br />
+	   
+	   <br/>
+	   <input type="submit" value="Envoyer" />
+   </p>
+</form>
+
+</body>
+</html>
+
---
+++ projet loic/mail/lien.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+<title>lien de validation</title>
+</head>
+
+Votre inscription a bien ete validee !!! :)
+
+<?php
+$validation = 1; 	//variable flag pour la validationde l'inscription. 
+					//Je pense l'utiliser pour "autoriser" le compte mais ya ptet mieux à faire.
+?>
+</html>
+
