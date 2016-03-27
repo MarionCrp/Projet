bb0b5315a646ff87bfb74e28577216880926b6d8
Marion Craipeau <marion_craipeau@hotmail.fr>
2016-03-27 21:41:41 +0200

    traduction en anglais du site (index et page d'édition du profil

--- view/edit_profile.php
+++ view/edit_profile.php
@@ -2,7 +2,7 @@
 
 	  	<div class="panel panel-default">
 		 	<div class="panel-heading">
-		   		 <h3 class="panel-title">Modifier votre compte</h3>
+		   		 <h3 class="panel-title"><?php echo _(" Edit you account "); ?></h3>
 		  	</div>
 		  
 		 	<div class="panel-body">
@@ -12,50 +12,50 @@
 
 	<div class="container">	
 		<form action="" method="post">
-			Nom : 
+			<?php echo _(" Name "); ?> : 
 				<input type ="text" name="name" maxlength="50" <?php echo 'value="'. $current_user->name() .'" ' ; ?>/>
-				<input type="submit" value="Modifier" name="edit_profile"/><br/>
+				<input type="submit" value=<?php echo _(" Edit "); ?> name="edit_profile"/><br/>
 		</form>
 
 		<form action="" method="post">
-			Email : 
+			Email Adress : 
 				<input type="text" name="email" maxlength="50" <?php echo 'value="' .$current_user->email().'"' ;?>/> 
-				<input type="submit" value="Modifier" name="edit_profile"/><br/>
+				<input type="submit" value=<?php echo _(" Edit "); ?> name="edit_profile"/><br/>
 		</form>
 
 		<form action="" method="post">
-			Sexe : 
+			<?php echo _(" Gender "); ?> : 
 				<select name="gender" id="gender">
 					<option value="male" 
-					<?php if ($current_user->gender() == 'male') echo 'selected'; ?> > Homme </option>
+					<?php if ($current_user->gender() == 'male') echo 'selected'; ?> > <?php echo _(" Male "); ?></option>
 					<option value="female" 
-					<?php if ($current_user->gender() == 'female') echo 'selected'; ?> > Femme </option>
+					<?php if ($current_user->gender() == 'female') echo 'selected'; ?> > <?php echo _(" Female "); ?></option>
 				</select>
-				<input type="submit" value="Modifier" name="edit_profile"/><br/>
+				<input type="submit" value=<?php echo _(" Edit "); ?> name="edit_profile"/><br/>
 		</form>
 	</div>
 
 	<div class="container">	
 		<form action="" method="post">
 
-			Mot de passe Actuel : 
+			<?php echo _(" Current Password "); ?> : 
 				<input type="password" name="current_password" maxlength="50"/><br/>
 
-			Nouveau mot de passe : 
+			<?php echo _(" New Password "); ?>
 				<input type="password" name="new_password" maxlength="50"/><br/>
 
-			Confirmer le mot de passe : 
+			<?php echo _(" Confirm Password "); ?> 
 				<input type="password" name="confirmed_pw" maxlength="50"/><br/> 
-			<input type="submit" value="Modifier le Mot de Passe" name="edit_password"/><br/>
+			<input type="submit" value=<?php echo _(" Edit password "); ?> name="edit_password"/><br/>
 
 		</form>
 	</div>
 
 
 		<form action="" method="post">
-			<p> Présentez-vous : <br/>
+			<p><?php echo _(" Tell more about yourself "); ?><br/>
 				<textarea name="description" id="description" rows="10" cols="60" /></textarea></p>
-				<input type="submit" value="Modifier ma description" name="edit_profile"/><br/>
+				<input type="submit" value=<?php echo _(" Edit your description "); ?> name="edit_profile"/><br/>
 		</form>
 		<!-- <form action="" method="post"><input type="submit" value="Supprimer mon compte" name="delete_account"></form> -->
 		 	</div><!-- Form-Panel-Body -->
